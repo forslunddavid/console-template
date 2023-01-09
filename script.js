42c8117ed0923e0b78c85100d85b0f762fe94cd0
David Andersson <lejonmanen@gmail.com>
2023-01-09 20:11:16 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+import { question } from 'readline-sync'
+
+let answer = question('What is your favorite color?')
+console.log(answer)
+
+let x = Number(question('What is your favorite number?'))
+console.log(x)
